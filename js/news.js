41f41931fae804878c0db4dab04b98c02dad201e
pulokmehrab <khandokarmeherab@gmail.com>
2022-09-03 01:07:02 +0600

    category-list

--- js/news.js
+++ js/news.js
@@ -1,5 +1,21 @@
                             // console.log("pulok")
                   
-   
+   const loadCategory = async() =>{
+    const url =`https://openapi.programming-hero.com/api/news/categories`
+    const res =await fetch(url);
+    const data =await res.json();
+    displayCategoris(data.data.news_category);
+}
+ const displayCategoris = newsTypes =>{
+    console.log(newsTypes);
+    const categorycontainer = document.getElementById('category-container');
+    for(const newsType of newsTypes){
+        const newsdiv =document.createElement('div');
+        newsdiv.innerHTML =`
+        <h5> ${newsType.category_name}</h5>`;
+        categorycontainer.appendChild(newsdiv);
+    }
+ }
+loadCategory();
  
                             
